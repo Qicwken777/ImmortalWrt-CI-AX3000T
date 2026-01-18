c1e257126908f156d054f0e1326c7a9dbf8a5b1c
Qicwken <qicwken@gmail.com>
2026-01-19 03:29:51 +0800

    Improve WiFi and DNS configuration in Settings.sh
    Refactor WiFi configuration updates and add DNS settings.

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -12,30 +12,35 @@ sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find .
 WIFI_SH=$(find ./target/linux/mediatek/filogic/base-files/etc/uci-defaults/ -type f -name "*set-wireless.sh" 2>/dev/null)
 WIFI_UC="./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
 if [ -f "$WIFI_SH" ]; then
-	#修改WIFI名称
-	sed -i "s/BASE_SSID='.*'/BASE_SSID='$WRT_SSID'/g" $WIFI_SH
-	#修改WIFI密码
-	sed -i "s/BASE_WORD='.*'/BASE_WORD='$WRT_WORD'/g" $WIFI_SH
+    #修改WIFI名称
+    sed -i "s/BASE_SSID='.*'/BASE_SSID='$WRT_SSID'/g" "$WIFI_SH"
+    #修改WIFI密码
+    if [ -n "$WRT_WORD" ]; then
+        sed -i "s/BASE_WORD='.*'/BASE_WORD='$WRT_WORD'/g" "$WIFI_SH"
+    fi
 elif [ -f "$WIFI_UC" ]; then
-	#修改WIFI名称
-	sed -i "s/ssid='.*'/ssid='$WRT_SSID'/g" $WIFI_UC
-	#修改WIFI密码
-	sed -i "s/key='.*'/key='$WRT_WORD'/g" $WIFI_UC
-	#修改WIFI地区
-	sed -i "s/country='.*'/country='CN'/g" $WIFI_UC
-	#修改WIFI加密
-	sed -i "s/encryption='.*'/encryption='psk2+ccmp'/g" $WIFI_UC
+    #修改WIFI名称
+    sed -i "s/ssid='.*'/ssid='$WRT_SSID'/g" "$WIFI_UC"
+    #修改WIFI地区
+    sed -i "s/country='.*'/country='CN'/g" "$WIFI_UC"
+    #修改WIFI加密
+    sed -i "s/encryption='.*'/encryption='none'/g" "$WIFI_UC"
 fi
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" "$CFG_FILE"
 #修改默认主机名
-sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" "$CFG_FILE"
 
 #修改opkg软件源为南京大学镜像站
-LAST_LINE=$(awk '/exit 0/{print NR}' $(find ./package/emortal/default-settings/files/ -type f -name "99-default-settings"))
-sed -i "${LAST_LINE}"'i\sed -i '\"s,https://downloads.immortalwrt.org,https://mirror.nju.edu.cn/immortalwrt,g\"' "/etc/opkg/distfeeds.conf"' $(find ./package/emortal/default-settings/files/ -type f -name "99-default-settings")
+DEFAULT_SETTINGS=$(find ./package/emortal/default-settings/files/ -type f -name "99-default-settings" 2>/dev/null | head -1)
+if [ -f "$DEFAULT_SETTINGS" ]; then
+    LAST_LINE=$(awk '/exit 0/{print NR}' "$DEFAULT_SETTINGS" | tail -1)
+    if [ -n "$LAST_LINE" ]; then
+        sed -i "${LAST_LINE}i\\"$'\n'"sed -i \"s,https://downloads.immortalwrt.org,https://mirror.nju.edu.cn/immortalwrt,g\" /etc/opkg/distfeeds.conf" "$DEFAULT_SETTINGS"
+    fi
+fi
 
 #配置文件修改
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
@@ -45,5 +50,62 @@ echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
 #手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
-	echo -e "$WRT_PACKAGE" >> ./.config
+    echo "$WRT_PACKAGE" >> ./.config
 fi
+
+# 添加dnsmasq设置，将miwifi.com解析到192.168.11.1
+DNSMASQ_CONF="./package/network/services/dnsmasq/files/dnsmasq.conf"
+if [ -f "$DNSMASQ_CONF" ]; then
+    echo "address=/miwifi.com/192.168.11.1" >> "$DNSMASQ_CONF"
+else
+    # 尝试其他可能的位置
+    DNSMASQ_CONF2="./package/base-files/files/etc/dnsmasq.conf"
+    if [ -f "$DNSMASQ_CONF2" ]; then
+        echo "address=/miwifi.com/192.168.11.1" >> "$DNSMASQ_CONF2"
+    elif [ -f "$DEFAULT_SETTINGS" ]; then
+        # 在默认设置中添加命令，让系统启动时写入
+        LAST_LINE=$(awk '/exit 0/{print NR}' "$DEFAULT_SETTINGS" | tail -1)
+        if [ -n "$LAST_LINE" ]; then
+            sed -i "${LAST_LINE}i\\"$'\n'"echo \"address=/miwifi.com/192.168.11.1\" >> /etc/dnsmasq.conf" "$DEFAULT_SETTINGS"
+        fi
+    fi
+fi
+
+# 移动UPnP页面从Services到Network
+if [ -f "./feeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json" ]; then
+    sed -i 's#admin/services/upnp#admin/network/upnp#g' \
+    ./feeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
+fi
+
+# 移动MosDNS页面从Services到Network
+if [ -f "./feeds/luci/applications/luci-app-mosdns/root/usr/share/luci/menu.d/luci-app-mosdns.json" ]; then
+    sed -i 's#admin/services/mosdns#admin/network/mosdns#g' \
+    ./feeds/luci/applications/luci-app-mosdns/root/usr/share/luci/menu.d/luci-app-mosdns.json
+fi
+
+# 从源码中读取版本信息
+DISTRIB_RELEASE=$(grep 'DISTRIB_RELEASE=' ./include/version.mk 2>/dev/null | cut -d '=' -f2 | tr -d " '")
+if [ -z "$DISTRIB_RELEASE" ]; then
+    # 尝试其他位置
+    DISTRIB_RELEASE=$(grep 'VERSION_NUMBER=' ./include/version.mk 2>/dev/null | cut -d '=' -f2 | tr -d " '")
+    if [ -z "$DISTRIB_RELEASE" ]; then
+        DISTRIB_RELEASE="$(date +%Y%m%d)"
+    fi
+fi
+
+# 创建banner目录（如果不存在）
+mkdir -p ./package/base-files/files/etc/
+
+# 修改banner
+cat << EOF > ./package/base-files/files/etc/banner
+     _________
+    /        /\      __  __                       _ 
+   /  MO    /  \    |  \/  | ___  _ __ ___   ___ (_)
+  /    MO  /    \   | |\/| |/ _ \| '_ ` _ \ / _ \| |
+ /________/  MO  \  | |  | | (_) | | | | | | (_) | |
+ \        \   MO /  |_|  |_|\___/|_| |_| |_|\___/|_|
+  \    MO  \    /  -------------------------------------------
+   \  MO    \  /    ImmortalWrt, ${DISTRIB_RELEASE}
+    \________\/    -------------------------------------------
+
+EOF
